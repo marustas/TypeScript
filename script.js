871dbce9254ced837ed66dbab7cda3b25c84bafb
Stas <marudov.stas@gmail.com>
2020-04-29 18:45:43 +0300

    Learned a new type 'any'

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 "use strict";
 console.log('Hello TypeScript');
+//let a : {text : string} = {text :'hello'}
+var b; //new type any
+b = 1; //new type any
+b = 'HELLO'; //new type any
+b = null; //new type any
+b = undefined; //new type any
+b = {}; //new type any
+b = function () { }; //new type any
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -1,4 +1,13 @@
 console.log('Hello TypeScript');
+//let a : {text : string} = {text :'hello'}
+let b : any;//new type any
+b = 1;//new type any
+b = 'HELLO'//new type any
+b = null//new type any
+b = undefined//new type any
+b= {}//new type any
+b =  function( )  { }//new type any
+
 
 
 
