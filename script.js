5e063cef74a0d05f88605e9f4871ff9169a12aad
Stas <marudov.stas@gmail.com>
2020-05-01 15:22:57 +0300

    learned new actions tha can be done with variables

--- script.js
+++ script.js
@@ -7,39 +7,37 @@
 //b = undefined//new type any
 //b= {}//new type any
 //b =  function( )  { }//new type any
-function a(x, y) {
-    return x + y;
-}
-;
-var b = function (x, y) { return x + y; };
-var c;
-c = function (x, y) {
-    return x + y;
-};
-var d = function (x, y) {
-    console.log(x + y);
-};
-var e = function (x) {
-    throw new Error(x);
-};
-var f = function (x, y) {
-    if (typeof x === 'number') {
-        return x + y;
-    }
-    else {
-        return '${x} ${y}';
-    }
-};
-console.log(f(1, 2));
-console.log(f("Answer", 3));
-function g(x, y) {
-    if (typeof x === 'number') {
-        return x + y;
-    }
-    else {
-        return '${x} ${y}';
-    }
-}
-;
-console.log(g(1, 2));
+//function a(x:number,y: number): number {
+//return x+y; 
+///};
+//let b=(x: number,y: number): number=> x+y;
+//let c:(x: number,y: number)=> number
+//c = function(x: number,y : number):number{
+//     return x + y
+// };
+// let d = function(x: number,y : number):void{
+//     console.log(x+y)
+// }
+//  let e =function(x:string): never{
+//      throw new Error(x)
+//  }
+// let f = function(x:any, y:any): any{
+//     if(typeof x === 'number'){
+//         return x+ y
+//     } else {
+//     return '${x} ${y}'
+//   }
+// };
+// console.log(f(1,2));
+// console.log(f("Answer",3));
+// function g(x: number,y: number): number;
+// function g(x: number,y: string): string;
+// function g(x:any, y:any): any{
+//     if(typeof x === 'number'){
+//         return x+ y
+//     } else {
+//     return '${x} ${y}'
+//   }
+// };
+//console.log(g(1,2))
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -7,40 +7,43 @@
 //b = undefined//new type any
 //b= {}//new type any
 //b =  function( )  { }//new type any
-function a(x:number,y: number): number {
-return x+y; 
-};
-let b=(x: number,y: number): number=> x+y;
+//function a(x:number,y: number): number {
+//return x+y; 
+///};
+//let b=(x: number,y: number): number=> x+y;
 
-let c:(x: number,y: number)=> number
-c = function(x: number,y : number):number{
-    return x + y
-};
-let d = function(x: number,y : number):void{
-    console.log(x+y)
-}
- let e =function(x:string): never{
-     throw new Error(x)
- }
+//let c:(x: number,y: number)=> number
+//c = function(x: number,y : number):number{
+//     return x + y
+// };
+// let d = function(x: number,y : number):void{
+//     console.log(x+y)
+// }
+//  let e =function(x:string): never{
+//      throw new Error(x)
+//  }
 
-let f = function(x:any, y:any): any{
-    if(typeof x === 'number'){
-        return x+ y
-    } else {
-    return '${x} ${y}'
-  }
-};
-console.log(f(1,2));
-console.log(f("Answer",3));
+// let f = function(x:any, y:any): any{
+//     if(typeof x === 'number'){
+//         return x+ y
+//     } else {
+//     return '${x} ${y}'
+//   }
+// };
+// console.log(f(1,2));
+// console.log(f("Answer",3));
 
-function g(x: number,y: number): number;
-function g(x: number,y: string): string;
+// function g(x: number,y: number): number;
+// function g(x: number,y: string): string;
 
-function g(x:any, y:any): any{
-    if(typeof x === 'number'){
-        return x+ y
-    } else {
-    return '${x} ${y}'
-  }
-};
-console.log(g(1,2))
+// function g(x:any, y:any): any{
+//     if(typeof x === 'number'){
+//         return x+ y
+//     } else {
+//     return '${x} ${y}'
+//   }
+// };
+//console.log(g(1,2))
+
+
+
