3bf1ef798431d976ccf558769bdfcce0320fef51
Stas <marudov.stas@gmail.com>
2020-05-02 19:25:51 +0300

    learned enums and tuples

--- script.js
+++ script.js
@@ -1,43 +1,11 @@
 "use strict";
-//let a : {text : string} = {text :'hello'}
-//let b : any;//new type any
-//b = 1;//new type any
-//b = 'HELLO'//new type any
-//b = null//new type any
-//b = undefined//new type any
-//b= {}//new type any
-//b =  function( )  { }//new type any
-//function a(x:number,y: number): number {
-//return x+y; 
-///};
-//let b=(x: number,y: number): number=> x+y;
-//let c:(x: number,y: number)=> number
-//c = function(x: number,y : number):number{
-//     return x + y
-// };
-// let d = function(x: number,y : number):void{
-//     console.log(x+y)
-// }
-//  let e =function(x:string): never{
-//      throw new Error(x)
-//  }
-// let f = function(x:any, y:any): any{
-//     if(typeof x === 'number'){
-//         return x+ y
-//     } else {
-//     return '${x} ${y}'
-//   }
-// };
-// console.log(f(1,2));
-// console.log(f("Answer",3));
-// function g(x: number,y: number): number;
-// function g(x: number,y: string): string;
-// function g(x:any, y:any): any{
-//     if(typeof x === 'number'){
-//         return x+ y
-//     } else {
-//     return '${x} ${y}'
-//   }
-// };
-//console.log(g(1,2))
+var b = [1, ''];
+b = [1];
+//enum
+var g;
+(function (g) {
+    g[g["a"] = 0] = "a";
+    g[g["b"] = 1] = "b";
+})(g || (g = {}));
+console.log(g.a, g.b);
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -1,49 +1,8 @@
-
-//let a : {text : string} = {text :'hello'}
-//let b : any;//new type any
-//b = 1;//new type any
-//b = 'HELLO'//new type any
-//b = null//new type any
-//b = undefined//new type any
-//b= {}//new type any
-//b =  function( )  { }//new type any
-//function a(x:number,y: number): number {
-//return x+y; 
-///};
-//let b=(x: number,y: number): number=> x+y;
-
-//let c:(x: number,y: number)=> number
-//c = function(x: number,y : number):number{
-//     return x + y
-// };
-// let d = function(x: number,y : number):void{
-//     console.log(x+y)
-// }
-//  let e =function(x:string): never{
-//      throw new Error(x)
-//  }
-
-// let f = function(x:any, y:any): any{
-//     if(typeof x === 'number'){
-//         return x+ y
-//     } else {
-//     return '${x} ${y}'
-//   }
-// };
-// console.log(f(1,2));
-// console.log(f("Answer",3));
-
-// function g(x: number,y: number): number;
-// function g(x: number,y: string): string;
-
-// function g(x:any, y:any): any{
-//     if(typeof x === 'number'){
-//         return x+ y
-//     } else {
-//     return '${x} ${y}'
-//   }
-// };
-//console.log(g(1,2))
-
-
-
+let b: [number, string?] = [1,'']
+b=[1]
+//enum
+enum g {
+    a,
+    b
+}
+console.log(g.a,g.b)
