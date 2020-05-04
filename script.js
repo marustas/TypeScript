91ad5df068440078585f20ff9b6a1d80f0f77d9d
Stas <marudov.stas@gmail.com>
2020-05-04 20:09:50 +0300

    Learned abstract classes and improved the previous code

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
 "use strict";
-var b = [1, ''];
-b = [1];
-//enum
-var g;
-(function (g) {
-    g[g["a"] = 0] = "a";
-    g[g["b"] = 1] = "b";
-})(g || (g = {}));
-console.log(g.a, g.b);
 //# sourceMappingURL=script.js.map
--- script.ts
+++
@@ -1,8 +0,0 @@
-let b: [number, string?] = [1,'']
-b=[1]
-//enum
-enum g {
-    a,
-    b
-}
-console.log(g.a,g.b)
